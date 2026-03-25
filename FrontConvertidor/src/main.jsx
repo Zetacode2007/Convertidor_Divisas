6262ec9722f3fc702faeb7e5f21d2283ba90ae9c
Informatica <informatica@alumno.local>
2026-03-25 17:06:56 -0600

    Cambios en Footer

--- FrontConvertidor/src/main.jsx
+++ FrontConvertidor/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import { NavBar } from './components/navbar/NavBar'
 import { Convertidor } from './components/convertidor/Convertidor'
 import { Footer } from './components/footer/Footer'
